2c3efcc664da4e31757f5dcb5181ed7848e45959
赵传运 <chuanyun.zhao@shenmajr.com>
2018-03-05 20:52:03 +0800

    refine
    Change-Id: If6054f249a78658d37a3c8e904598ed4aa22297d

--- flink-examples/src/main/java/com/coco/flink/examples/WordCountTable.java
+++ flink-examples/src/main/java/com/coco/flink/examples/WordCountTable.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.flink.table.examples.java;
+package com.coco.flink.examples;
 
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
@@ -26,60 +26,62 @@ import org.apache.flink.table.api.java.BatchTableEnvironment;
 
 /**
  * Simple example for demonstrating the use of the Table API for a Word Count in Java.
- *
+ * <p>
  * <p>This example shows how to:
- *  - Convert DataSets to Tables
- *  - Apply group, aggregate, select, and filter operations
+ * - Convert DataSets to Tables
+ * - Apply group, aggregate, select, and filter operations
  */
 public class WordCountTable {
 
-	// *************************************************************************
-	//     PROGRAM
-	// *************************************************************************
+    // *************************************************************************
+    //     PROGRAM
+    // *************************************************************************
 
-	public static void main(String[] args) throws Exception {
-		ExecutionEnvironment env = ExecutionEnvironment.createCollectionsEnvironment();
-		BatchTableEnvironment tEnv = TableEnvironment.getTableEnvironment(env);
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.createCollectionsEnvironment();
+        BatchTableEnvironment tEnv = TableEnvironment.getTableEnvironment(env);
 
-		DataSet<WC> input = env.fromElements(
-				new WC("Hello", 1),
-				new WC("Ciao", 1),
-				new WC("Hello", 1));
+        DataSet<WC> input = env.fromElements(
+                new WC("Hello", 1),
+                new WC("Ciao", 1),
+                new WC("Hello", 1));
 
-		Table table = tEnv.fromDataSet(input);
+        Table table = tEnv.fromDataSet(input);
 
-		Table filtered = table
-				.groupBy("word")
-				.select("word, frequency.sum as frequency")
-				.filter("frequency = 2");
+        Table filtered = table
+                .groupBy("word")
+                .select("word, frequency.sum as frequency")
+                .filter("frequency = 2");
 
-		DataSet<WC> result = tEnv.toDataSet(filtered, WC.class);
+        DataSet<WC> result = tEnv.toDataSet(filtered, WC.class);
 
-		result.print();
-	}
+        result.print();
+    }
 
-	// *************************************************************************
-	//     USER DATA TYPES
-	// *************************************************************************
+    // *************************************************************************
+    //     USER DATA TYPES
+    // *************************************************************************
 
-	/**
-	 * Simple POJO containing a word and its respective count.
-	 */
-	public static class WC {
-		public String word;
-		public long frequency;
+    /**
+     * Simple POJO containing a word and its respective count.
+     */
+    public static class WC {
+        public String word;
+        public long frequency;
 
-		// public constructor to make it a Flink POJO
-		public WC() {}
+        // public constructor to make it a Flink POJO
+        public WC() {
+        }
 
-		public WC(String word, long frequency) {
-			this.word = word;
-			this.frequency = frequency;
-		}
+        public WC(String word, long frequency) {
+            this.word = word;
+            this.frequency = frequency;
+        }
 
-		@Override
-		public String toString() {
-			return "WC " + word + " " + frequency;
-		}
-	}
+        @Override
+        public String toString() {
+            return "WC " + word + " " + frequency;
+        }
+    }
+
 }
