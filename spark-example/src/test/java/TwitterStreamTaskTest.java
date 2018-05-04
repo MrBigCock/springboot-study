234e1c3394845e7729b43c45e93348cadc8a2967
zhaochuanyun <chuanyun.zhao@shenmajr.com>
2018-05-04 20:01:26 +0800

    rm spark examples
    Change-Id: Icb67a4f8024e9ff3e4f7dcae054a3b8ec5276fbf

--- spark-example/src/test/java/TwitterStreamTaskTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import org.sparkexample.TwitterStreamTask;
-
-public class TwitterStreamTaskTest {
-    @Test
-    public void test() throws InterruptedException {
-        new TwitterStreamTask().run();
-    }
-}
